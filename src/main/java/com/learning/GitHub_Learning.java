905cea01ad9ed169506f777c91f00e7fbaa6ea08
Roopakinshu <roopakinshu@gmail.com>
2023-10-11 12:31:38 +0530

    branch1 third commit

--- src/main/java/com/learning/GitHub_Learning.java
+++ src/main/java/com/learning/GitHub_Learning.java
@@ -4,7 +4,11 @@ public class GitHub_Learning {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welocme to git");
+		System.out.println("Welocme to git");
+
+		int d = 40;
+		int e = 90;
+		System.out.println(d + e);
 	}
 
 }
